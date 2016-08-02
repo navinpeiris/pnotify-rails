d78b175d097e919f9e4326ab267f73e4695f5b5b
Nicolas Gonzalez <nicolas@mapotempo.com>
2016-08-02 10:08:57 +0200

    Load Material Icons Font From Assets

--- lib/pnotify-rails/engine.rb
+++ lib/pnotify-rails/engine.rb
@@ -1,6 +1,12 @@
 module PNotify
   module Rails
     class Engine < ::Rails::Engine
+      initializer 'assets.precompile', group: :all do |app|
+        app.config.assets.paths << root.join('assets', 'fonts').to_s
+        ['eot', 'woff', 'woff2', 'ttf'].each do |format|
+          app.config.assets.precompile << "pnotify-rails/MaterialIcons-Regular.#{format}"
+        end
+      end
     end
   end
 end
